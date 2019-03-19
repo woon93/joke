5a35dbd7dc22ad5376b86b7e971f7a4caf82e5bd
woon9 <woon93@163.com>
2019-03-19 23:48:21 +0800

    demo01

--- src/demo01.java
+++ src/demo01.java
@@ -1,4 +1,8 @@
+import java.io.File;
+
 public class demo01 {
-    public void main(String[] args){
+    public void main(String[] args) {
+        File file = new File("../resource/论文初稿.txt");
+//        InputStream fileInputStream = new FileInputStream("../src/resource/test01");
     }
 }
