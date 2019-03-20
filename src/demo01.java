3184057886af16f0a95a5290b3b38493e86a4eb1
RuanJianXinCheng <woon93@163.com>
2019-03-20 10:35:55 +0800

    天谷八路

--- src/demo01.java
+++ src/demo01.java
@@ -1,8 +1,28 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class demo01 {
-    public void main(String[] args) {
-        File file = new File("../resource/论文初稿.txt");
-//        InputStream fileInputStream = new FileInputStream("../src/resource/test01");
+
+    public static void main(String[] args) {
+        File file1 = new File("./resource/test01.txt");
+        File file2 = new File("E:/joke/resource/test01.txt");
+        try {
+            InputStream fs1 = new FileInputStream(file1);
+            System.out.println(fs1.read());
+
+            InputStream fs2 = new FileInputStream(file2);
+            System.out.println(fs2.read());
+
+
+            boolean eqs = fs1.equals(fs2);
+            System.out.println(eqs);
+
+            System.out.println(fs1.toString());
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
