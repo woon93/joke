35838feac0f0c22a1b94037fec2b51760b2e2b11
woon9 <woon93@163.com>
2019-03-21 23:04:21 +0800

    crawler test01

--- src/crawler01.java
+++ src/crawler01.java
@@ -16,9 +16,9 @@ public class crawler01 {
             String regex = "https://[\\w+\\.?/?]+\\.[A-Za-z]+";//url匹配规则
             Pattern p = Pattern.compile(regex);
             try {
-                url = new URL("https://www.cnblogs.com/qianzf/p/6796588.html");//爬取的网址、这里爬取的是一个生物网站
+                url = new URL("https://www.ifeng.com/");//爬取的网址、这里爬取的是一个生物网站
                 urlconn = url.openConnection();
-                pw = new PrintWriter(new FileWriter("./resource/crawler01.txt"), true);//将爬取到的链接放到D盘的SiteURL文件中
+                pw = new PrintWriter(new FileWriter("./resource/out/crawler01.txt"), true);//将爬取到的链接放到D盘的SiteURL文件中
                 br = new BufferedReader(new InputStreamReader(
                         urlconn.getInputStream()));
                 String buf = null;
--- src/demo01.java
+++ src/demo01.java
@@ -4,10 +4,10 @@ public class demo01 {
 
     public static void main(String[] args) {
         try {
-            File file1 = new File("./resource/论文初稿.txt");
+            File file1 = new File("./resource/out/论文初稿.txt");
             InputStream fs1 = new FileInputStream(file1);
             BufferedInputStream bf1 = new BufferedInputStream(fs1);
-            BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream("././resource/testOut.txt"));
+            BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream("resource/out/testOut.txt"));
             byte[] bytes = new byte[256];
             int len;
             while ((len = bf1.read(bytes)) != -1) {
