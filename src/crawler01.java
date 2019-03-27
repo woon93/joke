a1653ebd2e5287422534e7be896704704b77a736
RuanJianXinCheng <woon93@163.com>
2019-03-27 20:26:37 +0800

    天谷八路

--- src/crawler01.java
+++ src/crawler01.java
@@ -7,39 +7,41 @@ import java.util.regex.Pattern;
 
 public class crawler01 {
 
-        public static void main(String[] args) {
-            URL url = null;
-            URLConnection urlconn = null;
-            BufferedReader br = null;
-            PrintWriter pw = null;
-//        String regex = "http://[\\w+\\.?/?]+\\.[A-Za-z]+";
-            String regex = "https://[\\w+\\.?/?]+\\.[A-Za-z]+";//url匹配规则
-            Pattern p = Pattern.compile(regex);
-            try {
-                url = new URL("https://www.ifeng.com/");//爬取的网址、这里爬取的是一个生物网站
-                urlconn = url.openConnection();
-                pw = new PrintWriter(new FileWriter("./resource/out/crawler01.txt"), true);//将爬取到的链接放到D盘的SiteURL文件中
-                br = new BufferedReader(new InputStreamReader(
-                        urlconn.getInputStream()));
-                String buf = null;
-                while ((buf = br.readLine()) != null) {
-                    Matcher buf_m = p.matcher(buf);
-                    while (buf_m.find()) {
-                        pw.println(buf_m.group());
-                    }
+    public static void main(String[] args) {
+        URL url = null;
+        URLConnection urlconn = null;
+        BufferedReader br = null;
+        PrintWriter pw = null;
+        //url匹配规则
+        String regex = "https://[\\w+\\.?/?]+\\.[A-Za-z]+";
+        Pattern p = Pattern.compile(regex);
+        try {
+            //爬取的网址
+            url = new URL("https://www.ifeng.com/");
+            urlconn = url.openConnection();
+            //将爬取到的链接放到文件中
+            pw = new PrintWriter(new FileWriter("./resource/out/crawler01.txt"), true);
+            br = new BufferedReader(new InputStreamReader(
+                    urlconn.getInputStream()));
+            String buf = null;
+            while ((buf = br.readLine()) != null) {
+                Matcher buf_m = p.matcher(buf);
+                while (buf_m.find()) {
+                    pw.println(buf_m.group());
                 }
-                System.out.println("爬取成功^_^");
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
+            }
+            System.out.println("爬取成功^_^");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
             } catch (IOException e) {
                 e.printStackTrace();
-            } finally {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                pw.close();
             }
+            pw.close();
         }
+    }
 }
